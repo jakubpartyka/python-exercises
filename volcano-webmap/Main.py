9e2b840da3f70eb7085e8a84697859744a192365
Jakub Partyka <kontakt@jakub-partyka.pl>
2022-01-17 18:53:38 +0100

    Update Main.py

--- volcano-webmap/Main.py
+++ volcano-webmap/Main.py
@@ -30,3 +30,68 @@ fg.add_child(folium.GeoJson(data=open('data/world.json', 'r', encoding='utf-8-si
 map.add_child(fg)
 
 map.save('Map1.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=============================
+import random
+
+from PIL import Image, ImageColor
+
+
+def color(x_cord, y_cord, c=0):
+    if c == 0:
+        col = 'white'
+    else:
+        col = 'black'
+    im.putpixel((x_cord, y_cord), ImageColor.getcolor(col, '1'))  # or whatever color you wish
+
+
+# width and height of output image
+w = 24000
+h = 24000
+total_pixels = w * h
+
+log = True  # if true print a line every 1%
+
+im = Image.new('1', (w, h))  # create the Image of size 1 pixel
+
+counter = 0  # count how many pixels were drawn
+percent_completed = 0   # total progress in percent
+
+for x in range(w):
+    for y in range(h):
+        counter = counter + 1
+
+        # log progress
+        if log and counter % (total_pixels/100) == 0:
+            percent_completed = percent_completed + 1
+            print(percent_completed, "% done")
+
+        # decide on pixel color
+        k = random.randint(0, 1)
+        color(x, y, k)
+
+im.save('24000.png')  # or any image format
